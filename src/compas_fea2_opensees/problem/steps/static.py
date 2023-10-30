815c37149c8e8e216ca90e9527f84be0f0210850
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2023-10-30 16:33:34 +0100

    fix test units

--- src/compas_fea2_opensees/problem/steps/static.py
+++ src/compas_fea2_opensees/problem/steps/static.py
@@ -11,9 +11,10 @@ class OpenseesStaticStep(StaticStep):
     """
     __doc__ += StaticStep.__doc__
 
-    def __init__(self, max_increments=100, initial_inc_size=1, min_inc_size=0.00001, time=1, nlgeom=False, modify=True, name=None, **kwargs):
+    def __init__(self, max_increments=100, initial_inc_size=1, min_inc_size=0.00001, NormDispIncr=[1.0e-6, 10], time=1, nlgeom=False, modify=True, name=None, **kwargs):
         super(OpenseesStaticStep, self).__init__(max_increments, initial_inc_size,
                                                  min_inc_size, time, nlgeom, modify, name=name, **kwargs)
+        self.NormDispIncr=NormDispIncr
 
     def jobdata(self):
         return """#
@@ -37,7 +38,7 @@ class OpenseesStaticStep(StaticStep):
 constraints Transformation
 numberer RCM
 system BandGeneral
-test NormDispIncr 1.0e-6 6
+test NormDispIncr {} {}
 algorithm Newton
 integrator LoadControl 1
 
@@ -55,6 +56,7 @@ loadConst -time 0.0
                     self._generate_loads_section(),
                     self._generate_fields_section(),
                     self._generate_output_section(),
+                    self.NormDispIncr[0], self.NormDispIncr[1],
                     )
 
     def _generate_header_section(self):
