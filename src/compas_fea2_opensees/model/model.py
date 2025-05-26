1fc44723da935f76ec92b8568f85bed000f5004b
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2025-05-26 16:58:11 +0200

    solve installation from source issue

--- src/compas_fea2_opensees/model/model.py
+++ src/compas_fea2_opensees/model/model.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from compas_fea2.model import Model
 from compas_fea2.model import SolidSection
 from compas_fea2.model import TrussSection
